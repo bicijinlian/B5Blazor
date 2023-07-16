5a0c701f82a26401db344c124091e0980b598cbe
bicijinlian <bicijinlian@163.com>
2023-07-17 00:38:34 +0800

    feat: 使用示例

---
+++ B5Blazor.Shared/GlobalApp.cs
@@ -0,0 +1,9 @@
+﻿
+namespace B5Blazor.Shared;
+/// <summary>
+/// B5Blazor.Shared库 全局对象 
+/// </summary>
+public static class GlobalApp
+{ 
+    public static readonly string B5BlazorSharedWebRoot = $"_content/{typeof(GlobalApp).Assembly.GetName().Name}";
+}
---
+++ B5Blazor/GlobalApp.cs
@@ -0,0 +1,9 @@
+﻿
+using B5Blazor.Component;
+/// <summary>
+/// B5Blazor UI库 全局对象 
+/// </summary>
+public static class GlobalApp
+{ 
+    public static readonly string B5BlazorUiWebRoot = $"_content/{typeof(B5BlazorBase).Assembly.GetName().Name}";
+}
